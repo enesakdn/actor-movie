53ad445f46446cf05d64a5fd732f6dca27d305d5
enesakdn <enesakdogan948@gmail.com>
2023-10-03 18:08:23 +0300

    Initial commit

---
+++ src/main/java/com/actormovie/actormovie/ActorMovieApplication.java
@@ -0,0 +1,13 @@
+package com.actormovie.actormovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActorMovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActorMovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/actormovie/actormovie/ActorMovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actormovie.actormovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActorMovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
